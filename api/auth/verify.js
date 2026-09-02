64ebf4b6ef1c2dbcf461726fbcfba5fedb4880f7
Terav2 <tera100000000@gmail.com>
2026-09-02 21:18:34 +0530

    Update Redis configuration environment variables

--- api/auth/verify.js
+++ api/auth/verify.js
@@ -2,8 +2,8 @@ import { Redis } from '@upstash/redis';
 import jwt from 'jsonwebtoken';
 
 const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
+  url: process.env.Sio_KV_REST_API_URL,
+  token: process.env.Sio_KV_REST_API_TOKEN,
 });
 
 export default async function handler(req, res) {
