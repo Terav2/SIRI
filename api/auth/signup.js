4a2d9154a849b24f0ff506591d85e8a89b121efb
Terav2 <tera100000000@gmail.com>
2026-09-02 21:17:51 +0530

    Update Redis configuration for signup handler

--- api/auth/signup.js
+++ api/auth/signup.js
@@ -3,8 +3,8 @@ import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 
 const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
+  url: process.env.Sio_KV_REST_API_URL,
+  token: process.env.Sio_KV_REST_API_TOKEN,
 });
 
 export default async function handler(req, res) {
